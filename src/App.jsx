d37270195805bea0012e09b52c78659a10690f4d
Sahil Rajurkar <sahilrajurkar88@gmail.com>
2025-03-08 22:53:26 +0530

    added home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import SocialLinks from "./components/SocialLinks"
 import "./index.css"
    
 const App = () => {
     return (
         <>
         <Navbar/>
+        <Home/>
+        
+        <SocialLinks/>
         </>
     )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import profile from '../assets/profile.jpeg'
+import { HiArrowSmRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'  >
+
+       
+    
+
+        <div  className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'   >
+            <div  className='flex flex-col justify-center h-full'   >
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'  >I'm a Full Stack Developer</h2>
+                <p  className='text-gray-500 py-4 max-w-md'  >
+                I specialize in Java, Spring Boot, Hibernate, ReactJS, and MySQL.  
+  
+  Passionate about backend development, I’m eager to learn and contribute to impactful projects.
+
+                </p>
+
+                <div>
+                    <button className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-purple-600 cursor-pointer '  >
+                    
+                    Portfolio
+
+                    <span className='group-hover:rotate-90 duration-300 '  >
+
+                    <HiArrowSmRight size={20} className='ml-1'   />
+                    </span>
+                    </button>
+                </div>
+            </div>
+            <div>
+                 <img src={profile} className=' rounded-2xl mx-auto
+                 w-2/3   '  alt='my profile'  />
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
