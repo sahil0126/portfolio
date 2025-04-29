5f67656986f95f4f5ef343d3653d71a5cc23b0f1
Sahil Rajurkar <sahilrajurkar88@gmail.com>
2025-04-30 00:29:11 +0530

    projects section started

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,30 @@
 import React from "react"
+import expense_tracker from "../assets/expense_tracker.jpg"
 
 const Projects = () => {
     return (
-        <>
+        <div>
 
-        <h2>
-            Projects
-        </h2>
+        <div>
+            <div>
+                <p>Projects</p>
+                <p>Check out some of my work right here</p>
+            </div>
+
+            <div>
+                <div>
+                    <img src={expense_tracker} alt="" />
+                    <div>
+                        <button>Demo</button>
+                        <button>Code</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+   
         
-        </>
+        </div>
     )
 }
 
